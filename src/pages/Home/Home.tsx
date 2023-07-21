b6e294c2988d776bff59dae29c85445f2ab88ff1
ardguezsoc <adevsocorro@gmail.com>
2023-07-22 01:23:13 +0200

    feat: adding handleFileContent logistic in home view

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import { Text } from '../../components/Text';
 import { FileHandle } from '../../components/FileHandle/FileHandle';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { sliceArrayBuffer } from '../../utils/arrayBufferUtils';
 
+export interface OffsetController {
+  offset: number;
+  offsetChunk: number;
+}
 export const Home = () => {
-  const navigate = useNavigate();
   const { t } = useTranslation();
+  const [arrayBuffer, setArrayBuffer] = useState(new ArrayBuffer(0));
+  const [fileContent, setFileContent] = useState('');
+  const [offsetController, setOffsetController] = useState<OffsetController>({ offset: -1, offsetChunk: 0 });
+
+  const handleFileContent = async (file: File) => {
+    const auxArrayBuffer = await file.arrayBuffer();
+    setArrayBuffer(auxArrayBuffer);
+    sliceArrayBuffer(1, offsetController, auxArrayBuffer, setOffsetController, setFileContent);
+  };
+
   return (
     <div>
       <Text>{t('home')}</Text>
-      <h1 onClick={() => navigate('/history')}>Home</h1>
-      <FileHandle />
+      <FileHandle onChange={handleFileContent} />
     </div>
   );
 };
