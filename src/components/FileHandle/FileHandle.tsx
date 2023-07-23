cfdd9d92f0658aec3d4a9b27c2924b57fe737d11
ardguezsoc <adevsocorro@gmail.com>
2023-07-23 14:12:06 +0200

    feat: drag and drop files2

--- src/components/FileHandle/FileHandle.tsx
+++ src/components/FileHandle/FileHandle.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useHistory } from '../../hooks';
 import { fileInfoHandler } from './utils/fileInfoHandler';
+import { Text } from '../Text';
+import { Button } from '../Button';
+import { Box } from '@mui/material';
+import { StyledFileHandleContainer } from './FileHandle.style';
+import { useDropzone } from 'react-dropzone';
+import { useTranslation } from 'react-i18next';
+import { theme } from '../../themes/themes';
 
 interface FileHandleProps {
   onChange: (file: File) => void;
@@ -8,9 +15,20 @@ interface FileHandleProps {
 
 export const FileHandle: React.FC<FileHandleProps> = ({ onChange }) => {
   const { setHistoryOfFiles } = useHistory();
+  const inputRef = useRef<HTMLInputElement>(null); // Specify the type of the ref here
+  const { t } = useTranslation();
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const file = event?.target?.files?.[0];
+  const onDrop = (droppedFile: File[]) => handleFileChange(undefined, droppedFile[0]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      text: ['.txt'],
+    },
+  });
+
+  const handleFileChange = (event?: React.ChangeEvent<HTMLInputElement>, fileDropped?: File): void => {
+    const file = event?.target?.files?.[0] || fileDropped;
     if (file) {
       setHistoryOfFiles(fileInfoHandler(file));
       onChange(file);
@@ -18,8 +36,18 @@ export const FileHandle: React.FC<FileHandleProps> = ({ onChange }) => {
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-    </div>
+    <StyledFileHandleContainer $isDragActive={isDragActive}>
+      <Box id="fileHandleDrag" {...getRootProps()}>
+        <input ref={inputRef} type="file" onChange={handleFileChange} {...getInputProps()} />{' '}
+        <Box id="fileHandleTextContainer">
+          <Text fontVariant="h6">{isDragActive ? t('dragnDrop.dragnDropFiles') : t('dragnDrop.dropFilesHere')}</Text>
+          <Box>
+            <Text fontVariant="h6">{t('or')}</Text>
+            <Button onClick={() => inputRef.current?.click()}>{t('button.uploadFile')}</Button>
+          </Box>
+          <Text color={theme.text.notFound}>{t('dragnDrop.conditions')}</Text>
+        </Box>
+      </Box>
+    </StyledFileHandleContainer>
   );
 };
