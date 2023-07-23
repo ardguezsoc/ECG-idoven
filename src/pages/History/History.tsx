500065acdcc7d9000d814e5603db73ffb112746c
ardguezsoc <adevsocorro@gmail.com>
2023-07-23 14:11:45 +0200

    feat: backArrow in history

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -2,10 +2,13 @@ import { useHistory } from '../../hooks';
 import { Table } from '../../components/Table';
 import { Text } from '../../components/Text';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { BackArrow } from '../../components/BackArrow';
 
 export const History = () => {
   const { history } = useHistory();
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const headers = [
     t('table.headers.name'),
     t('table.headers.size'),
@@ -14,5 +17,10 @@ export const History = () => {
     t('table.headers.extension'),
   ];
 
-  return history.length ? <Table rows={history} headers={headers} /> : <Text>{t('errors.noFilesAdded')}</Text>;
+  return (
+    <>
+      <BackArrow goBack={() => navigate('/')} />
+      {history.length ? <Table rows={history} headers={headers} /> : <Text>{t('errors.noFilesAdded')}</Text>}
+    </>
+  );
 };
